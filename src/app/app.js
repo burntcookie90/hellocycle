08a6a385f2dfc448587a664cabcb86f7a6c93834
Vishnu Rajeevan <vishnurajeevan@gmail.com>
2015-09-18 23:14:27 -0400

    initial commit

--- src/app/app.js
+++ src/app/app.js
@@ -1,21 +1,62 @@
 import Cycle from '@cycle/core';
 import {makeDOMDriver, h} from '@cycle/dom';
+import {makeHTTPDriver} from '@cycle/http';
+const HACKER_NEWS_URL = 'https://whispering-fortress-7282.herokuapp.com/';
 
-const title = 'Cycle.js Starter';
+function renderStoryCell(story){
+    return h('div.story-details', [
+            h('h1.story-title', story.title),
+            h('h4.story-user', story.user),
+            h('a.story-url', {href: story.url}, story.url)
+    ])
+}
 
-function main (responses) {
+function requestStory(itemId){
+    return {
+        url: HACKER_NEWS_URL + 'item/' + String(itemId),
+        method: 'GET'
+    };
+}
 
-  return {
-    DOM: Cycle.Rx.Observable
-      .just(title)
-      .map(title =>
-        h('h1', title)
-      )
-  }
+function model(actions){
+    return Cycle.Rx.Observable.combineLatest(
+            actions.click$.map(requestStory())
+            );
+}
+
+function view(state$) {
+    return state$.map(({story}) =>
+            h('div', [
+                h('button.get-story', 'Get Story'),
+                story === null ? null : renderStoryCell(story)
+            ])
+            );
+}
+
+function intent(DOM, HTTP) {
+    return {
+        click$: DOM.select('.get-story').events('click'),
+        story$: HTTP.filter(res$ => res$.request.url.indexOf(HACKER_NEWS_URL) === 0)
+            .mergeAll()
+            .map(res => res.body)
+            .startWith(null)
+    };
+}
+
+function main ({DOM, HTTP}) {
+
+    let actions = intent(DOM, HTTP);
+    let state$ = model(actions);
+
+    return {
+        DOM: view(model(intent(DOM, HTTP))),
+        HTTP: state$
+    };
 }
 
 let drivers = {
-  DOM: makeDOMDriver('#app')
+    DOM: makeDOMDriver('#app'),
+    HTTP: makeHTTPDriver()
 }
 
 Cycle.run(main, drivers);
