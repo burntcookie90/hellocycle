0fb40e54a7957e960b78df9087cec46ec9dcb783
Vishnu Rajeevan <vishnurajeevan@gmail.com>
2015-09-19 00:21:38 -0400

    who knows hwat i'm doing

--- src/app/app.js
+++ src/app/app.js
@@ -6,6 +6,7 @@ const HACKER_NEWS_URL = 'https://whispering-fortress-7282.herokuapp.com/';
 
 function renderStoryCell(story) {
     console.log("renderStoryCell");
+    console.dir(story);
     return h('div.story-details', [
         h('h1.story-title', story.title),
         h('h4.story-user', story.user),
@@ -23,30 +24,34 @@ function requestStory(itemId) {
 
 function model(actions) {
     console.log("model");
-    return Cycle.Rx.Observable.combineLatest(
-        actions.click$.map(requestStory())
+    let state$ = Cycle.Rx.Observable.combineLatest(
+        actions.click$.map(requestStory()),
+        actions.story$
         );
+    return state$;
 }
 
 function view(state$) {
     console.log("view");
-    return state$.map(({story}) =>
+    let vtree$ = state$.map(({story}) =>
         h('div', [
             h('button.get-story', 'Get Story'),
             story === null ? null : renderStoryCell(story)
         ])
         );
+    return vtree$;
 }
 
 function intent(DOM, HTTP) {
     console.log("intent")
-    return {
+    let actions = {
         click$: DOM.select('.get-story').events('click'),
         story$: HTTP.filter(res$ => res$.request.url.indexOf(HACKER_NEWS_URL) === 0)
             .mergeAll()
             .map(res => res.body)
             .startWith(null)
     };
+    return actions;
 }
 
 function main({DOM, HTTP}) {
